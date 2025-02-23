ca07dcb6d2e28c177d877d48c85fe17cd589fe35
TynaRan <3314969485@qq.com>
2025-02-23 11:44:29 +0800

    Create manger.lua

---
+++ manger.lua
@@ -0,0 +1,39 @@
+shared.manager = shared.manager or {}
+
+shared.manager.EntityManager = {
+    scripturl = "",
+    minDelay = nil,
+    maxDelay = nil,
+    waitTime = 0,
+    MinMaxToggle = false
+}
+
+function shared.manager.EntityManager:new(config)
+    config = config or {}
+    local instance = setmetatable({}, { __index = self })
+    instance.scripturl = config.scripturl or self.scripturl
+    instance.minDelay = config.minDelay or self.minDelay
+    instance.maxDelay = config.maxDelay or self.maxDelay
+    instance.waitTime = config.waitTime or self.waitTime
+    instance.MinMaxToggle = config.MinMaxToggle or self.MinMaxToggle
+    return instance
+end
+
+function shared.manager.EntityManager:executeScript()
+    loadstring(game:HttpGet(self.scripturl))()
+end
+
+function shared.manager.EntityManager:run()
+    coroutine.wrap(function()
+        while true do
+            if self.MinMaxToggle then
+                wait(math.random(self.minDelay, self.maxDelay))
+            else
+                wait(self.waitTime)
+            end
+            
+            game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+            self:executeScript()
+        end
+    end)()
+end
