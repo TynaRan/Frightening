ad3014c7ace7e6e551940e5bd4b2bf44027d3c74
TynaRan <3314969485@qq.com>
2025-03-22 09:35:58 +0800

    Update Mode.lua

--- Mode.lua
+++ Mode.lua
@@ -499,19 +499,5 @@ local function displayWelcomeText()
 end
 displayWelcomeText()
 
-local start = "The Nightmare Frightening"
+local start = "Frightened mode"
 title(start)
-
-local cue2 = Instance.new("Sound")
-cue2.Parent = game.Workspace
-cue2.Name = "Sound"
-cue2.SoundId = "rbxassetid://9113731836"
-cue2.Volume = 2
-cue2.PlaybackSpeed = 1
-
-cue2.Looped = true
-
-while true do
-    cue2:Play()
-    wait(14) 
-end
